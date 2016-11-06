941f7e94bef8a2f40f9b53198df5b938de4a97ad
Maria <mmorrison1978@gmail.com>
2016-11-06 12:12:29 -0700

    more functions

--- 004/004.js
+++ 004/004.js
@@ -1,9 +1,23 @@
-function clear() {
-  document.getElementById('clear').value = ' ';
+var entered = [];
+var total = 0;
 
-}
+document.getElementById('clear').onclick = function () {
+  document.getElementById('screen').value = "";
+};
+
+document.getElementById('allClear').onclick = function () {
+  document.getElementById('screen').value = "";
+};
 
 
-  document.getElementById('one').onclick = function () {
-    document.getElementById('screen').value = '1';
-  }
+document.getElementById('one').onclick = function () {
+  document.getElementById('screen').value = '1';
+};
+
+document.getElementById('two').onclick = function () {
+  document.getElementById('screen').value = '2';
+};
+
+document.getElementById('three').onclick = function () {
+  document.getElementById('screen').value = '3';
+};
