4e1e224e7651b07d5ce8b9f4090c790c7be12e42
Maria <mmorrison1978@gmail.com>
2016-11-06 16:18:34 -0700

    wired up number buttons

--- 004/004.js
+++ 004/004.js
@@ -9,8 +9,9 @@ document.getElementById('allClear').onclick = function () {
   document.getElementById('screen').value = "";
 };
 
-
 document.getElementById('one').onclick = function () {
+  entered.push(1);
+  console.log(entered);
   document.getElementById('screen').value = '1';
 };
 
@@ -20,4 +21,32 @@ document.getElementById('two').onclick = function () {
 
 document.getElementById('three').onclick = function () {
   document.getElementById('screen').value = '3';
+};
+
+document.getElementById('four').onclick = function () {
+  document.getElementById('screen').value = '4';
+};
+
+document.getElementById('five').onclick = function () {
+  document.getElementById('screen').value = '5';
+};
+
+document.getElementById('six').onclick = function () {
+  document.getElementById('screen').value = '6';
+};
+
+document.getElementById('seven').onclick = function () {
+  document.getElementById('screen').value = '7';
+};
+
+document.getElementById('eight').onclick = function () {
+  document.getElementById('screen').value = '8';
+};
+
+document.getElementById('nine').onclick = function () {
+  document.getElementById('screen').value = '9';
+};
+
+document.getElementById('zero').onclick = function () {
+  document.getElementById('screen').value = '0';
 };
