da908b5f83ae22afba00ec52ad40ea71326b2148
Maria <mmorrison1978@gmail.com>
2016-11-13 21:22:24 -0700

    changed everything

--- 007/007.js
+++ 007/007.js
@@ -1,10 +1,5 @@
 $(document).ready(function() {
-  // $('#ColorChanger').slideToggle('slow', function(){
-  //   $('.container').css('scheme1')
-  // });
-
-  $('#FontChanger').slideToggle('slow', function () {
-    $('body').css("font-family", "\ 'Ranga\'")
+  $('.settings').click(function () {
+    $('.ChangeFont').toggleClass('.showHide')
   })
-
 })
