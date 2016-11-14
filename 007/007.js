23d0c1ecb3aa584f56c71f224dcf8b9f3570a9b6
Maria <mmorrison1978@gmail.com>
2016-11-13 21:04:03 -0700

    added jquery to change font

--- 007/007.js
+++ 007/007.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
-  $('#ColorChanger').slideToggle('slow', function(){
-    $('body').addClass('scheme1')
+  // $('#ColorChanger').slideToggle('slow', function(){
+  //   $('.container').css('scheme1')
+  // });
+
+  $('#FontChanger').slideToggle('slow', function () {
+    $('body').css("font-family", "\ 'Ranga\'")
   })
+
 })
