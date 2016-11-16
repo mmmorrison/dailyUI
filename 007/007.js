a42d8ea348d7edcb0a672065f27b25d9a9f47e1e
Maria <mmorrison1978@gmail.com>
2016-11-16 07:17:54 -0700

    trying to wire up the color scheme buttons

--- 007/007.js
+++ 007/007.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // Click fonts button and shows font options
   $('.fonts').click(function () {
     if ($('.ChangeFont').css('visibility') == 'hidden'){
       $('.ChangeFont').css('visibility', 'visible');
@@ -6,4 +7,15 @@ $(document).ready(function() {
       $('.ChangeFont').css('visibility', 'hidden');
     }
   })
-  })
+
+  // Click colors button and shows color options
+  $('.colors').click(function () {
+    if ($('.ChangeColor').css('visibility') == 'hidden'){
+      $('.ChangeColor').css('visibility', 'visibile');
+    }else{
+      $('.ChangeColor').css('visibile', 'hidden');
+    }
+  });
+
+
+})
