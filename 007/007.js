70591724e23b14d64922f56ce02072d94f441f3c
Maria <mmorrison1978@gmail.com>
2016-11-16 20:28:34 -0700

    fixed visibility issue on change font div

--- 007/007.js
+++ 007/007.js
@@ -6,16 +6,18 @@ $(document).ready(function() {
     }else{
       $('.ChangeFont').css('visibility', 'hidden');
     }
-  })
+  });
 
   // Click colors button and shows color options
   $('.colors').click(function () {
     if ($('.ChangeColor').css('visibility') == 'hidden'){
-      $('.ChangeColor').css('visibility', 'visibile');
+      $('.ChangeColor').css('visibility', 'visible');
     }else{
-      $('.ChangeColor').css('visibile', 'hidden');
+      $('.ChangeColor').css('visibility', 'hidden');
     }
   });
 
 
+
 })
+x
