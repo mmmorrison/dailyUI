fa90f738ddfdc58dc6dea27a463a9647e3306df8
Maria <mmorrison1978@gmail.com>
2016-11-18 22:08:22 -0700

    check font buttons work

--- 007/007.js
+++ 007/007.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   // Click fonts button and shows font options
-  $('.fontsButton').click(function () {
+  $('.fontsButton').click(function() {
     if ($('.ChangeFont').css('visibility') == 'hidden'){
       $('.ChangeFont').css('visibility', 'visible');
     }else{
@@ -9,7 +9,7 @@ $(document).ready(function() {
   });
 
   // Click colors button and shows color options
-  $('.colorsButton').click(function () {
+  $('.colorsButton').click(function() {
     if ($('.ChangeColor').css('visibility') == 'hidden'){
       $('.ChangeColor').css('visibility', 'visible');
     }else{
@@ -17,6 +17,13 @@ $(document).ready(function() {
     }
   });
 
+  //Click only one box
+  $('.fontBoxes').click(function() {
+    if ($(this).is(':checked')) {
+      $('.fontBoxes').not($(this)).attr('checked', false);
+      $('body').css('font-family', this.name);
 
+    }
+  });
 
 })
