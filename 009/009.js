5fcddd3455783350014aff7a35f84d2c84ae6870
Maria <mmorrison1978@gmail.com>
2016-11-19 10:47:09 -0700

    wiring up some jquery or javascript

---
+++ 009/009.js
@@ -0,0 +1,20 @@
+var albumInfo = [{
+ cover: 'oasis_beherenow.jpg',
+ artist: 'Oasis',
+ album: 'Be Here Now'
+}, {
+ cover: 'sade_thebestofsade.jpg',
+ artist: 'Sade',
+ album: 'The Best of Sade'
+}, {
+ cover: 'thebeatles_meetthebeatles.jpg',
+ artist: 'The Beatles',
+ album: 'Meet The Beatles'
+}, {
+ cover: 'thedoors_anamericanprayer.jpg',
+ artist: 'The Doors',
+ albums: 'An American Prayer'
+}]
+
+
+console.log("<img src='albums/'" + albumInfo[0].cover);
