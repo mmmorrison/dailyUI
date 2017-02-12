b720d2c9c2f9ae67a3ff0210a8d29588bfb53224
Maria <mmorrison1978@gmail.com>
2017-02-11 19:27:20 -0700

    changed background and show success message

--- 011/011.js
+++ 011/011.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
   $('.btn').click(function () {
-    
+    $('.buttonHolder').hide();
+    $('.spinner').show().delay(5000).queue(function () {
+      $(this).hide();
+      $('body').css('background-color', '#728F81');
+      $('.message').show();
+    })
   })
 })
