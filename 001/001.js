e93cef2e8094a9a20f0802ff640f3920c7e9909c
Maria <mmorrison1978@gmail.com>
2017-02-12 20:33:40 -0700

    added randomizer

--- 001/001.js
+++
@@ -1,4 +0,0 @@
-// document.write("test")
-// $(document).ready(function() {
-//  console.log("jQuery is wired up");
-// })
--- 011/011.js
+++ 011/011.js
@@ -1,10 +1,21 @@
 $(document).ready(function () {
+  var random = Math.floor(Math.random() * $('.message').length);
+
   $('.btn').click(function () {
     $('.buttonHolder').hide();
     $('.spinner').show().delay(5000).queue(function () {
       $(this).hide();
-      $('body').css('background-color', '#728F81');
-      $('.message').show();
+      $('.message').hide().eq(random).show();
     })
   })
-})
+
+  
+
+
+});
+
+
+
+
+
+// $('body').css('background-color', '#728F81');
